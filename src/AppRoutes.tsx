b10a41e543e30db18f3fe15f8637a963efbbc8c7
zeyqam <zegamda@code.edu.az>
2024-07-24 22:28:14 +0400

    Frontend setup and header section

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout>Home Page</Layout>} />
+      <Route path="/user-profile" element={<span>User Profile Page</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
