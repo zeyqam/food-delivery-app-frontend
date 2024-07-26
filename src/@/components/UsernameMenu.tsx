0dab2194c44b11acbbb97d511c16ebb5e616dfe8
zeyqam <zegamda@code.edu.az>
2024-07-26 11:12:46 +0400

    login fixed

---
+++ src/@/components/UsernameMenu.tsx
@@ -0,0 +1,41 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Separator } from "@radix-ui/react-separator";
+import { CircleUserRound } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
+const UsernameMenu = () => {
+  const { user, logout } = useAuth0();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
+        <CircleUserRound className="text-orange-500" />
+        {user ? `Hello, ${user.email}` : "Hello, Guest"}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white shadow-lg rounded-md mt-2 p-2">
+        <DropdownMenuItem className="px-2 py-1">
+          <Link to="/user-profile" className="font-bold hover:text-orange-500">
+            User Profile
+          </Link>
+        </DropdownMenuItem>
+        <Separator className="my-1" />
+        <DropdownMenuItem className="px-2 py-1">
+          <Button
+            onClick={() => logout()}
+            className="w-full font-bold text-white bg-orange-500 hover:bg-orange-600"
+          >
+            Log Out
+          </Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UsernameMenu;
