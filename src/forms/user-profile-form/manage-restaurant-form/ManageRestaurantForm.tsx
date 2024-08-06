5c9d9f60ec1db9cfb54ecac662b49af4cdf9619c
zeyqam <zegamda@code.edu.az>
2024-08-06 15:35:02 +0400

    create restaurant form

--- src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -5,6 +5,10 @@ import { z } from "zod";
 import DetailsSection from "./DetailsSection";
 import { Separator } from "@/@/components/ui/separator";
 import CuisinesSection from "./CuisinesSection";
+import MenuSection from "./MenuSection";
+import ImageSection from "./ImageSection";
+import LoadingButton from "@/@/components/LoadingButton";
+import { Button } from "@/@/components/ui/button";
 const formSchema = z.object({
   restaurantName: z.string({
     required_error: "restaurant name is required",
@@ -63,6 +67,11 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
         <DetailsSection />
         <Separator />
         <CuisinesSection />
+        <Separator />
+        <MenuSection />
+        <Separator />
+        <ImageSection />
+        {isLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
       </form>
     </Form>
   );
