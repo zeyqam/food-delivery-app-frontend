839d49cdc3b9c2e6df1a7f1c9ff271e6534e6f26
zeyqam <zegamda@code.edu.az>
2024-08-07 16:29:30 +0400

    create restaurant api request

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,6 +1,11 @@
-import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-  return <ManageRestaurantForm />;
+  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  return (
+    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+  );
 };
+
 export default ManageRestaurantPage;
