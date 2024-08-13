6e015d911f2ffa9b847289d406902e32c1c445e8
zeyqam <zegamda@code.edu.az>
2024-08-13 04:09:54 +0400

    checkoutButton fixed

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -4,9 +4,10 @@ import { AspectRatio } from "@/@/components/ui/aspect-ratio";
 import RestaurantInfo from "@/@/components/RestaurantInfo";
 import MenuItem from "@/@/components/MenuItem";
 import { useState } from "react";
-import { Card } from "@/@/components/ui/card";
+import { Card, CardFooter } from "@/@/components/ui/card";
 import OrderSummary from "@/@/components/OrderSummary";
 import { MenuItem as MenuItemType } from "@/types";
+import CheckoutButton from "@/@/components/CheckoutButton";
 
 export type CartItem = {
   _id: string;
@@ -19,7 +20,10 @@ const DetailPage = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
 
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
+    return storedCartItems ? JSON.parse(storedCartItems) : [];
+  });
 
   const addToCart = (menuItem: MenuItemType) => {
     setCartItems((prevCartItems) => {
@@ -46,6 +50,12 @@ const DetailPage = () => {
           },
         ];
       }
+
+      sessionStorage.setItem(
+        `cartItems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
+      );
+
       return updatedCartItems;
     });
   };
@@ -54,6 +64,11 @@ const DetailPage = () => {
     setCartItems((prevCartItems) => {
       const updatedCartItems = prevCartItems.filter(
         (item) => cartItem._id !== item._id
+      );
+
+      sessionStorage.setItem(
+        `cartItems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
       );
       return updatedCartItems;
     });
@@ -89,6 +104,9 @@ const DetailPage = () => {
               cartItems={cartItems}
               removeFromCart={removeFromCart}
             />
+            <CardFooter>
+              <CheckoutButton />
+            </CardFooter>
           </Card>
         </div>
       </div>
